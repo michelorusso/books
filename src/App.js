c77eb0e2fa78714d07a73650d292988d760a6971
Michele Lorusso <miky-l@hotmail.it>
2023-02-13 16:12:43 +0100

    complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import BookCreate from "./Components/BookCreate";
+import BookList from "./Components/BookList";
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  const editBookById = (id, newTitle) => {
+    // eslint-disable-next-line
+    const updateBooks = books.map((book) => {
+      if (book.id === id) {
+        return { ...book, title: newTitle };
+      }
+    });
+    setBooks(updateBooks);
+  };
+
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+
+    setBooks(updatedBooks);
+  };
+
+  const handleBookCreate = (title) => {
+    const updatedBooks = [...books, { id: Date.now(), title: title }];
+    setBooks(updatedBooks);
+  };
+
   return (
-    <>
-      <h1>hello</h1>
-    </>
+    <div className="app">
+      <BookList books={books} onDelete={deleteBookById} onEdit={editBookById} />
+      <BookCreate onCreate={handleBookCreate} />
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
+import "./style/index.css";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const el = document.getElementById("root");
+const root = ReactDOM.createRoot(el);
+
+// ---
+// const root = ReactDOM.createRoot(document.getElementById("root"))
 
 root.render(<App />);

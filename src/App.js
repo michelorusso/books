014c1cd9e0a82e79025a017d7cb44d6b736ef94d
Michele Lorusso <miky-l@hotmail.it>
2023-02-20 10:55:40 +0100

    refactor with context

--- src/App.js
+++ src/App.js
@@ -1,51 +1,10 @@
-import React, { useState, useEffect } from "react";
+import { useEffect, useContext } from "react";
 import BookCreate from "./Components/BookCreate";
 import BookList from "./Components/BookList";
-import axios from "axios";
+import BooksContext from "./context/books";
 
 function App() {
-  const [books, setBooks] = useState([]);
-
-  const fetchBooks = async () => {
-    const respone = await axios.get("http://localhost:3001/books");
-
-    setBooks(respone.data);
-  };
-
-  const editBookById = async (id, newTitle) => {
-    const response = await axios.put(`http://localhost:3001/books/${id}`, {
-      title: newTitle,
-    });
-
-    // eslint-disable-next-line
-    const updateBooks = books.map((book) => {
-      if (book.id === id) {
-        return { ...book, ...response.data };
-      }
-      return book;
-    });
-    setBooks(updateBooks);
-  };
-
-  const deleteBookById = async (id) => {
-    await axios.delete(`http://localhost:3001/books/${id}`);
-
-    const updatedBooks = books.filter((book) => {
-      return book.id !== id;
-    });
-
-    setBooks(updatedBooks);
-  };
-
-  const handleBookCreate = async (title) => {
-    const response = await axios.post("http://localhost:3001/books", {
-      title,
-    });
-
-    const updatedBooks = [...books, response.data];
-
-    setBooks(updatedBooks);
-  };
+  const { fetchBooks, books } = useContext(BooksContext);
 
   useEffect(() => {
     fetchBooks();
@@ -53,8 +12,9 @@ function App() {
 
   return (
     <div className="app">
-      <BookList books={books} onDelete={deleteBookById} onEdit={editBookById} />
-      <BookCreate onCreate={handleBookCreate} />
+      <h1>Reading List</h1>
+      <BookList />
+      <BookCreate />
     </div>
   );
 }
